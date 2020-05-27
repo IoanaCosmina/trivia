75097c0be1d293145e727d084c0537a71aa1f894
unknown <andriescuioana@gmail.com>
2020-05-27 10:10:27 +0200

    setting up Question content from OpenTriviaDB

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Category from './components/Category';
 import Score from './components/Score';
@@ -6,14 +6,28 @@ import Question from './components/Question';
 import Result from './components/Result';
 
 function App() {
+  const [question, setQuestion] = useState(null);
+  useEffect(() => {
+    getQuestion();
+  }, []);
+
+  function getQuestion() {
+    const url = 'https://opentdb.com/api.php?amount=1';
+
+    fetch(url)
+      .then(res => res.json())
+      .then((data) => setQuestion(data.results[0]));
+  }
+
   return (
     <div className="app">
+      {/* <Result /> */}
       <div className="question-header">
         <Category />
         <Score />
       </div>
       <div className="question-main">
-        <Question />
+        {question && <Question question={question} />}
       </div>
       <div className="question-footer">
         <button>Next question</button>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import shuffle from 'lodash.shuffle';
 
-const dummyAnswers = ['Answer A', 'Answer B', 'Answer C', 'Answer D'];
+function Question({ question }) {
+    const answers = shuffle([...question.incorrect_answers, question.correct_answer]);
+    console.log(answers)
 
-function Question() {
     return (
         <div className="question">
-            <h2>Question Text</h2>
-            {dummyAnswers.map((answer, index) => (
-                <button key={index}>{answer}</button>
+            <h2 dangerouslySetInnerHTML={{ __html: question.question }} />
+            {answers.map((answer, index) => (
+                <button key={index} dangerouslySetInnerHTML={{ __html: answer }} />
             ))}
         </div>
     );
