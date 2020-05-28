d367222b82c52f68eb837d85d4ec0b0ebdd5964c
unknown <andriescuioana@gmail.com>
2020-05-28 10:31:13 +0200

    Updates

---
+++ precache-manifest.7dba3bfd1281c257f3904160fc68487d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ac6b51edd30120851fc42cb25f21a5",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "6d5a1d8a4db5ec710856",
+    "url": "/trivia/static/css/main.7e466027.chunk.css"
+  },
+  {
+    "revision": "59e7bdbe4d896b95eed0",
+    "url": "/trivia/static/js/2.aed8fc93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trivia/static/js/2.aed8fc93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5a1d8a4db5ec710856",
+    "url": "/trivia/static/js/main.a8ec7c21.chunk.js"
+  },
+  {
+    "revision": "f40d299287008199bdc4",
+    "url": "/trivia/static/js/runtime-main.c48cb86f.js"
+  }
+]);
